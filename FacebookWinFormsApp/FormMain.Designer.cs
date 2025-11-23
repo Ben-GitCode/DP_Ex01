730f97d008e752af0ab7637455d8ae954d9921ef
Adi <Aditapiro12@gmail.com>
2025-11-23 13:50:55 +0200

    style and decoration for FormMain

--- FacebookWinFormsApp/FormMain.Designer.cs
+++ FacebookWinFormsApp/FormMain.Designer.cs
@@ -1,16 +1,22 @@
-﻿namespace BasicFacebookFeatures
-{
-    partial class FormMain
-    {
-        /// <summary>
-        /// Required designer variable.
-        /// </summary>
-        private System.ComponentModel.IContainer components = null;
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
 
-        /// <summary>
-        /// Clean up any resources being used.
-        /// </summary>
-        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+namespace BasicFacebookFeatures
+{
+    partial class FormMain : Form
+    {
+        private System.ComponentModel.IContainer components = null;
+        private Button buttonLogin;
+        private Button buttonLogout;
+        private Label label1;
+        private TabControl tabControl1;
+        private TabPage tabPage1;
+        private TabPage tabPage2;
+        private TextBox textBoxAppID;
+        private PictureBox pictureBoxProfile;
+        private Button buttonConnectAsDesig;
+
         protected override void Dispose(bool disposing)
         {
             if (disposing && (components != null))
@@ -20,14 +26,9 @@
             base.Dispose(disposing);
         }
 
-        #region Windows Form Designer generated code
-
-        /// <summary>
-        /// Required method for Designer support - do not modify
-        /// the contents of this method with the code editor.
-        /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.buttonLogin = new System.Windows.Forms.Button();
             this.buttonLogout = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
@@ -41,136 +42,130 @@
             this.tabPage1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBoxProfile)).BeginInit();
             this.SuspendLayout();
-            // 
-            // buttonLogin
-            // 
-            this.buttonLogin.Location = new System.Drawing.Point(18, 17);
-            this.buttonLogin.Margin = new System.Windows.Forms.Padding(4);
-            this.buttonLogin.Name = "buttonLogin";
-            this.buttonLogin.Size = new System.Drawing.Size(268, 44);
-            this.buttonLogin.TabIndex = 36;
-            this.buttonLogin.Text = "Login";
-            this.buttonLogin.UseVisualStyleBackColor = true;
-            this.buttonLogin.Click += new System.EventHandler(this.buttonLogin_Click);
-            // 
-            // buttonLogout
-            // 
-            this.buttonLogout.Enabled = false;
-            this.buttonLogout.Location = new System.Drawing.Point(18, 121);
-            this.buttonLogout.Margin = new System.Windows.Forms.Padding(4);
-            this.buttonLogout.Name = "buttonLogout";
-            this.buttonLogout.Size = new System.Drawing.Size(268, 43);
-            this.buttonLogout.TabIndex = 52;
-            this.buttonLogout.Text = "Logout";
-            this.buttonLogout.UseVisualStyleBackColor = true;
-            this.buttonLogout.Click += new System.EventHandler(this.buttonLogout_Click);
-            // 
+
+            // FormMain Styling
+            this.Font = new Font("Segoe UI", 11F);
+            this.BackColor = Color.FromArgb(59, 89, 152); // Facebook blue
+
             // label1
-            // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(314, 17);
-            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Location = new Point(314, 17);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(465, 78);
+            this.label1.Size = new Size(465, 78);
             this.label1.TabIndex = 53;
-            this.label1.Text = "This is the AppID of \"Design Patterns App 2.4\".\r\nThe grader will use it to test y" +
-    "our app.\r\nType here your own AppID to test it:\r\n";
-            // 
+            this.label1.Text = "This is the AppID of 'Design Patterns App 2.4'.\r\nThe grader will use it to test your app.\r\nType here your own AppID to test it:";
+            this.label1.ForeColor = Color.White;
+            this.label1.Font = new Font("Segoe UI", 12F, FontStyle.Bold);
+
             // tabControl1
-            // 
             this.tabControl1.Controls.Add(this.tabPage1);
             this.tabControl1.Controls.Add(this.tabPage2);
-            this.tabControl1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.tabControl1.Location = new System.Drawing.Point(0, 0);
+            this.tabControl1.Dock = DockStyle.Fill;
+            this.tabControl1.Location = new Point(0, 0);
             this.tabControl1.Name = "tabControl1";
             this.tabControl1.SelectedIndex = 0;
-            this.tabControl1.Size = new System.Drawing.Size(862, 370);
+            this.tabControl1.Size = new Size(862, 370);
             this.tabControl1.TabIndex = 54;
-            // 
+
             // tabPage1
-            // 
             this.tabPage1.Controls.Add(this.buttonConnectAsDesig);
             this.tabPage1.Controls.Add(this.pictureBoxProfile);
             this.tabPage1.Controls.Add(this.textBoxAppID);
             this.tabPage1.Controls.Add(this.label1);
             this.tabPage1.Controls.Add(this.buttonLogout);
             this.tabPage1.Controls.Add(this.buttonLogin);
-            this.tabPage1.Location = new System.Drawing.Point(4, 35);
+            this.tabPage1.Location = new Point(4, 35);
             this.tabPage1.Name = "tabPage1";
-            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage1.Size = new System.Drawing.Size(854, 331);
+            this.tabPage1.Padding = new Padding(3);
+            this.tabPage1.Size = new Size(854, 331);
             this.tabPage1.TabIndex = 0;
-            this.tabPage1.Text = "tabPage1";
+            this.tabPage1.Text = "Welcome";
             this.tabPage1.UseVisualStyleBackColor = true;
-            // 
-            // pictureBoxProfile
-            // 
-            this.pictureBoxProfile.Location = new System.Drawing.Point(18, 171);
-            this.pictureBoxProfile.Name = "pictureBoxProfile";
-            this.pictureBoxProfile.Size = new System.Drawing.Size(79, 78);
-            this.pictureBoxProfile.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
-            this.pictureBoxProfile.TabIndex = 55;
-            this.pictureBoxProfile.TabStop = false;
-            // 
-            // textBoxAppID
-            // 
-            this.textBoxAppID.Location = new System.Drawing.Point(319, 126);
-            this.textBoxAppID.Name = "textBoxAppID";
-            this.textBoxAppID.Size = new System.Drawing.Size(446, 32);
-            this.textBoxAppID.TabIndex = 54;
-            this.textBoxAppID.Text = "1282715863617766"; // Updated App ID
-            // 
+            this.tabPage1.BackColor = Color.White;
+
             // tabPage2
-            // 
-            this.tabPage2.Location = new System.Drawing.Point(4, 35);
+            this.tabPage2.Location = new Point(4, 35);
             this.tabPage2.Name = "tabPage2";
-            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage2.Size = new System.Drawing.Size(752, 327);
+            this.tabPage2.Padding = new Padding(3);
+            this.tabPage2.Size = new Size(752, 327);
             this.tabPage2.TabIndex = 1;
-            this.tabPage2.Text = "tabPage2";
+            this.tabPage2.Text = "Other";
             this.tabPage2.UseVisualStyleBackColor = true;
-            // 
+            this.tabPage2.BackColor = Color.White;
+
+            // buttonLogin
+            this.buttonLogin.Location = new Point(18, 17);
+            this.buttonLogin.Name = "buttonLogin";
+            this.buttonLogin.Size = new Size(268, 44);
+            this.buttonLogin.TabIndex = 36;
+            this.buttonLogin.Text = "Login";
+            this.buttonLogin.FlatStyle = FlatStyle.Flat;
+            this.buttonLogin.BackColor = Color.White;
+            this.buttonLogin.ForeColor = Color.FromArgb(59, 89, 152);
+            this.buttonLogin.Font = new Font("Segoe UI", 11F, FontStyle.Bold);
+            this.buttonLogin.UseVisualStyleBackColor = true;
+            this.buttonLogin.Click += new EventHandler(this.buttonLogin_Click);
+
             // buttonConnectAsDesig
-            // 
-            this.buttonConnectAsDesig.Location = new System.Drawing.Point(18, 69);
-            this.buttonConnectAsDesig.Margin = new System.Windows.Forms.Padding(4);
+            this.buttonConnectAsDesig.Location = new Point(18, 69);
             this.buttonConnectAsDesig.Name = "buttonConnectAsDesig";
-            this.buttonConnectAsDesig.Size = new System.Drawing.Size(268, 44);
+            this.buttonConnectAsDesig.Size = new Size(268, 44);
             this.buttonConnectAsDesig.TabIndex = 56;
             this.buttonConnectAsDesig.Text = "Connect As Desig";
+            this.buttonConnectAsDesig.FlatStyle = FlatStyle.Flat;
+            this.buttonConnectAsDesig.BackColor = Color.LightSkyBlue;
+            this.buttonConnectAsDesig.ForeColor = Color.White;
+            this.buttonConnectAsDesig.Font = new Font("Segoe UI", 11F, FontStyle.Bold);
             this.buttonConnectAsDesig.UseVisualStyleBackColor = true;
-            this.buttonConnectAsDesig.Click += new System.EventHandler(this.buttonConnectAsDesig_Click);
-            // 
+            this.buttonConnectAsDesig.Click += new EventHandler(this.buttonConnectAsDesig_Click);
+
+            // buttonLogout
+            this.buttonLogout.Enabled = false;
+            this.buttonLogout.Location = new Point(18, 121);
+            this.buttonLogout.Name = "buttonLogout";
+            this.buttonLogout.Size = new Size(268, 43);
+            this.buttonLogout.TabIndex = 52;
+            this.buttonLogout.Text = "Logout";
+            this.buttonLogout.FlatStyle = FlatStyle.Flat;
+            this.buttonLogout.BackColor = Color.WhiteSmoke;
+            this.buttonLogout.ForeColor = Color.Gray;
+            this.buttonLogout.Font = new Font("Segoe UI", 11F, FontStyle.Bold);
+            this.buttonLogout.UseVisualStyleBackColor = true;
+            this.buttonLogout.Click += new EventHandler(this.buttonLogout_Click);
+
+            // pictureBoxProfile
+            this.pictureBoxProfile.Location = new Point(18, 171);
+            this.pictureBoxProfile.Name = "pictureBoxProfile";
+            this.pictureBoxProfile.Size = new Size(79, 78);
+            this.pictureBoxProfile.SizeMode = PictureBoxSizeMode.Zoom;
+            this.pictureBoxProfile.TabIndex = 55;
+            this.pictureBoxProfile.TabStop = false;
+            this.pictureBoxProfile.BackColor = Color.White;
+            this.pictureBoxProfile.BorderStyle = BorderStyle.FixedSingle;
+
+            // textBoxAppID
+            this.textBoxAppID.Location = new Point(319, 126);
+            this.textBoxAppID.Name = "textBoxAppID";
+            this.textBoxAppID.Size = new Size(446, 32);
+            this.textBoxAppID.TabIndex = 54;
+            this.textBoxAppID.Text = "1282715863617766";
+            this.textBoxAppID.BackColor = Color.WhiteSmoke;
+            this.textBoxAppID.Font = new Font("Segoe UI", 12F);
+
             // FormMain
-            // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(13F, 26F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(862, 370);
+            this.AutoScaleDimensions = new SizeF(13F, 26F);
+            this.AutoScaleMode = AutoScaleMode.Font;
+            this.ClientSize = new Size(862, 370);
             this.Controls.Add(this.tabControl1);
-            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.Margin = new System.Windows.Forms.Padding(4);
-            this.Name = "FormMain";
-            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            this.Text = "Form1";
+            this.FormBorderStyle = FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.StartPosition = FormStartPosition.CenterScreen;
+            this.Text = "Facebook Features";
             this.tabControl1.ResumeLayout(false);
             this.tabPage1.ResumeLayout(false);
             this.tabPage1.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBoxProfile)).EndInit();
             this.ResumeLayout(false);
-
         }
-
-		#endregion
-
-		private System.Windows.Forms.Button buttonLogin;
-		private System.Windows.Forms.Button buttonLogout;
-		private System.Windows.Forms.Label label1;
-		private System.Windows.Forms.TabControl tabControl1;
-		private System.Windows.Forms.TabPage tabPage1;
-		private System.Windows.Forms.TabPage tabPage2;
-        private System.Windows.Forms.TextBox textBoxAppID;
-        private System.Windows.Forms.PictureBox pictureBoxProfile;
-        private System.Windows.Forms.Button buttonConnectAsDesig;
     }
 }
-
