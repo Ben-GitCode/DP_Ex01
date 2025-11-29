a0a0fb8d37562a895ffee0fa5c0eceb7d7fbe747
adi0258 <aditapiro12@gmail.com>
2025-11-29 19:53:23 +0200

    merge

--- FacebookWinFormsApp/FormMain.cs
+++ FacebookWinFormsApp/FormMain.cs
@@ -258,22 +258,22 @@ namespace BasicFacebookFeatures
         // ---------------- DARK MODE ----------------
         private void toggleDarkMode_Click(object sender, EventArgs e)
         {
-            isDarkMode = !isDarkMode;
+            m_isDarkMode = !m_isDarkMode;
             applyDarkMode();
 
             // Move toggle circle
-            toggleCircle.Left = isDarkMode ? 26 : 1;
+            toggleCircle.Left = m_isDarkMode ? 26 : 1;
 
             // Change background color
-            toggleBackground.BackColor = isDarkMode ? Color.DarkGray : Color.LightGray;
+            toggleBackground.BackColor = m_isDarkMode ? Color.DarkGray : Color.LightGray;
         }
 
         private void applyDarkMode()
         {
-            Color formColor = isDarkMode ? Color.Black : Color.White;
-            Color textColor = isDarkMode ? Color.White : Color.Black;
-            Color listBoxBackColor = isDarkMode ? Color.Black : Color.White;
-            Color listBoxForeColor = isDarkMode ? Color.White : Color.Black;
+            Color formColor = m_isDarkMode ? Color.Black : Color.White;
+            Color textColor = m_isDarkMode ? Color.White : Color.Black;
+            Color listBoxBackColor = m_isDarkMode ? Color.Black : Color.White;
+            Color listBoxForeColor = m_isDarkMode ? Color.White : Color.Black;
 
             tabPageLogin.BackColor = formColor;
 
@@ -293,7 +293,7 @@ namespace BasicFacebookFeatures
         {
             if (control is Button b)
             {
-                b.BackColor = isDarkMode ? Color.FromArgb(50, 50, 50) : Color.FromArgb(66, 103, 178);
+                b.BackColor = m_isDarkMode ? Color.FromArgb(50, 50, 50) : Color.FromArgb(66, 103, 178);
                 b.ForeColor = Color.White;
             }
             else if (control is LinkLabel ll)
