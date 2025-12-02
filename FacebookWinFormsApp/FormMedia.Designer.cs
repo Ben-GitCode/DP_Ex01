867ef4c2f80ca32f1397cdcdcc609e29eada8054
adi0258 <aditapiro12@gmail.com>
2025-12-02 12:34:13 +0200

    ready

--- FacebookWinFormsApp/FormMedia.Designer.cs
+++ FacebookWinFormsApp/FormMedia.Designer.cs
@@ -28,14 +28,14 @@ namespace BasicFacebookFeatures
 
         private Button buttonBack;
 
-        protected override void Dispose(bool disposing)
+        protected override void Dispose(bool i_Disposing)
         {
-            if (disposing && (components != null))
+            if (i_Disposing && (components != null))
             {
                 components.Dispose();
             }
 
-            base.Dispose(disposing);
+            base.Dispose(i_Disposing);
         }
 
         private void InitializeComponent()
