a7f6ae7331e2237eacdbb507df96c8b05ef9ce46
adi0258 <aditapiro12@gmail.com>
2026-01-07 10:15:42 +0200

    branches named changed

--- FacebookWinFormsApp/ColorPalette.cs
+++ FacebookWinFormsApp/ColorPalette.cs
@@ -5,16 +5,11 @@ namespace BasicFacebookFeatures
     public static class ColorPalette
     {
         public static readonly Color sr_FacebookBlue = Color.FromArgb(66, 103, 178);
-
         public static readonly Color sr_White = Color.White;
-
         public static readonly Color sr_Black = Color.Black;
         public static readonly Color sr_DarkGray = Color.FromArgb(36, 37, 38);
-
         public static readonly Color sr_LightGray = Color.DarkGray;
-
         public static readonly Color sr_WhitishBlue = Color.FromArgb(180, 200, 255);
-
         public static readonly Color sr_DarkBlue = Color.FromArgb(12, 36, 86);
         public static readonly Color sr_MidGray = Color.FromArgb(90, 90, 110);
     }
